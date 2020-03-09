19cad477c620ea3c04a02d76e0502956d0e0b01c
Will Lyons <wdlyons1@gmail.com>
2020-03-09 10:45:13 -0400

    Add files via upload

---
+++ Assignment3/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="external.css">
+  </head>
+  <body>
+<?php
+echo "My first PHP script!";
+?>
+  </body>
+
+</html>
